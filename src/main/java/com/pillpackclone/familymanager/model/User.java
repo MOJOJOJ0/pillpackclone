5a67a8cc49369cf618497965067c7bd5b134ab51
Ronel Peter <ronel.sajan@pm.me>
2022-02-01 02:58:50 -0600

    added annotations

--- src/main/java/com/pillpackclone/familymanager/model/User.java
+++ src/main/java/com/pillpackclone/familymanager/model/User.java
@@ -1,18 +1,17 @@
 package com.pillpackclone.familymanager.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity // ! this class mapped to any db
-public class User implements Serializable { // ! transforms this class into different types of strings (db to json..)
+@Entity // * this class mapped to any db
+public class User implements Serializable { // + transforms this class into different types of strings (db to json..)
     @Id // ! ENTITY NEEDS PRIMARY KEY
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // ! how to gen this info, pass in strategy to be specific
+    @GeneratedValue(strategy = GenerationType.AUTO) // ? how to gen this info, pass in strategy to be specific
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
+    @Column(nullable = false, updatable = false) // ! once set cannot be updated
     private String phoneNumber;
     private String imageUrl;
 
