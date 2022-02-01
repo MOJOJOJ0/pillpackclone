cf249a81d5940a08c540a518ce5f6f176f74bdaa
Ronel Peter <ronel.sajan@pm.me>
2022-02-01 02:53:24 -0600

    primary key setup

--- src/main/java/com/pillpackclone/familymanager/model/User.java
+++ src/main/java/com/pillpackclone/familymanager/model/User.java
@@ -1,7 +1,19 @@
 package com.pillpackclone.familymanager.model;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity // ! this class mapped to any db
+public class User implements Serializable { // ! transforms this class into different types of strings (db to json..)
+    @Id // ! ENTITY NEEDS PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ! how to gen this info, pass in strategy to be specific
     private Long id;
     private String name;
-    
+    private String email;
+    private String phoneNumber;
+    private String imageUrl;
+
 }
