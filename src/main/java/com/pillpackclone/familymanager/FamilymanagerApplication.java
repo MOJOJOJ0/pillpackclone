d5e41b363af7a80dfa8c61010ba93faa4c75c6c7
Ronel Peter <ronel.sajan@pm.me>
2022-02-01 02:25:59 -0600

    initialized spring

---
+++ src/main/java/com/pillpackclone/familymanager/FamilymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.pillpackclone.familymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilymanagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pillpackclone/familymanager/model/User.java
@@ -0,0 +1,5 @@
+package com.pillpackclone.familymanager.model;
+
+public class User {
+    private Long id;
+}
