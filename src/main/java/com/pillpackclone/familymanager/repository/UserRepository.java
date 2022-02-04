a6e298151afda89d14be23751059759a2ee50b49
Ronel Peter <ronel.sajan@pm.me>
2022-02-04 16:54:13 -0600

    cleaned up user repo file

--- src/main/java/com/pillpackclone/familymanager/repository/UserRepository.java
+++ src/main/java/com/pillpackclone/familymanager/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    void deleteUserById(Long id); // ! long pk type, query method coz naming convention, method, class name, field, spring will create query
+    void deleteUserById(Long id);
 
-    Optional<User> findUserById(Long id); // ! find whatever that's passed in there, spring query method, since random id therefore optional
+    Optional<User> findUserById(Long id);
 }
