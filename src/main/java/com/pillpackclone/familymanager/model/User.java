6d873186ecf2cc477296d70d813106704bc38473
Ronel Peter <ronel.sajan@pm.me>
2022-02-04 16:46:56 -0600

    cleaned up user model

--- src/main/java/com/pillpackclone/familymanager/model/User.java
+++ src/main/java/com/pillpackclone/familymanager/model/User.java
@@ -3,15 +3,15 @@ package com.pillpackclone.familymanager.model;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity // * this class mapped to any db
-public class User implements Serializable { // ! transforms this class into different types of strings (db to json..)
-    @Id // ! ENTITY NEEDS PRIMARY KEY
-    @GeneratedValue(strategy = GenerationType.AUTO) // ! how to gen this info, pass in strategy to be specific
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
-    @Column(nullable = false, updatable = false) // ! once set cannot be updated
+    @Column(nullable = false, updatable = false) 
     private String phoneNumber;
     //private String imageUrl;
 
